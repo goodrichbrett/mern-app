cc04dc65b7344dd5787b65bb518e5a1c61f63ffa
goodrichbrett <brettgoodrich@outlook.com>
2020-09-17 10:27:38 -0500

    add todo form

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+class AddTodo extends Component {
+	state = {
+		invalidForm: true,
+		formData: {
+			name: '',
+			isDone: false,
+			dueDate: Date.now(),
+		},
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.handleAddMovie(this.state.formData);
+	};
+
+	handleChange = (e) => {
+		const formData = {
+			...this.state.formData,
+			[e.target.name]: e.target.value,
+		};
+		this.setState({
+			formData,
+			invalidForm: !this.formRef.current.checkValidity(),
+		});
+	};
+
+	formRef = React.createRef();
+	render() {
+		return (
+			<>
+				<div className="AddTodo">
+					<Form ref={this.formRef} onSubmit={this.handleSubmit}>
+						<FormGroup>
+							<Label for="name">Todo</Label>
+							<Input
+								onChange={this.handleChange}
+								value={this.state.formData.name}
+								type="text"
+								name="name"
+								id="name"
+								required
+							></Input>
+						</FormGroup>
+						<FormGroup>
+							<Label for="isDone">Is Done?</Label>
+							<Input
+								onChange={this.handleChange}
+								value={this.state.formData.isDone}
+								type="text"
+								name="isDone"
+								id="isDone"
+							></Input>
+						</FormGroup>
+						<FormGroup>
+							<Label for="dueDate">Due Date</Label>
+							<Input
+								onChange={this.handleChange}
+								value={this.state.formData.dueDate}
+								type="date"
+								name="dueDate"
+								id="dueDate"
+							></Input>
+						</FormGroup>
+					</Form>
+				</div>
+			</>
+		);
+	}
+}
+
+export default AddTodo;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ToDo from '../../components/ToDo/ToDo';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-const TodoList = ({user, todoList}) => {
-    return (
-    <>
-        <Link to={'/create'} activeClassName="active">New To-Do</Link>
+const TodoList = ({ user, todoList }) => {
+	return (
+		<>
+			<Link to={'todos/add'}>New To-Do</Link>
 
-        {todoList.map(todo => (
-            <ToDo key={todo.id} name={todo.name} isDone={todo.isDone} dueDate={todo.dueDate}/>
-        ))
-        }
-    </>
-    );
-}
- 
+			{todoList.map((todo) => (
+				<ToDo
+					key={todo.id}
+					name={todo.name}
+					isDone={todo.isDone}
+					dueDate={todo.dueDate}
+				/>
+			))}
+		</>
+	);
+};
+
 export default TodoList;
