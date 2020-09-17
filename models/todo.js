68ad51abc2157151fcf6e8ac2a141c6359689050
goodrichbrett <brettgoodrich@outlook.com>
2020-09-17 08:38:17 -0500

    add todoSchema

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	isDone: {
+		type: Boolean,
+		default: false,
+	},
+	dueDate: {
+		type: Date,
+		default: Date.now(),
+	},
+});
