0a2fca2fbe090b5bfd0260e6b169db3facf5326c
dscription <dscriptionplayground@gmail.com>
2020-09-17 11:38:24 -0400

    create services

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ToDo from '../../components/ToDo/ToDo';
-import { Link } from 'react-router-dom';
-
-const TodoList = ({ user, todoList }) => {
-	return (
-		<>
-			<Link to={'todos/add'}>New To-Do</Link>
-
-			{todoList.map((todo) => (
-				<ToDo
-					key={todo.id}
-					name={todo.name}
-					isDone={todo.isDone}
-					dueDate={todo.dueDate}
-				/>
-			))}
-		</>
-	);
-=======
 import React from "react";
 import ToDo from "../../components/ToDo/ToDo";
 import { Link } from "react-router-dom";
@@ -41,7 +20,6 @@ const TodoList = ({ user, todoList }) => {
       ))}
     </>
   );
->>>>>>> 71687d2000f40498fd6e460f94061f32317718e4
 };
 
 export default TodoList;
---
+++ src/services/todoService.js
@@ -0,0 +1,33 @@
+import tokenService from '../services/tokenService';
+const BASE_URL = '/api/todos/';
+
+export function create(todo) {
+    return fetch(BASE_URL, {
+        method: "POST",
+        headers: { 'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken() },
+        body: JSON.stringify(todo)
+    }, { mode: "cors" })
+        .then(res => res.json());
+}
+
+export function getAll() {
+    return fetch(BASE_URL, {mode: "cors"})
+    .then(res => res.json())
+  }
+
+  export function deleteOne(id) {
+    return fetch(`${BASE_URL}${id}`, {
+        method: 'DELETE',
+        headers: {'Authorization': 'Bearer ' + tokenService.getToken()}
+    }, {mode: "cors"})
+    .then(res => res.json());
+  }
+
+export function update(todo) {
+    return fetch(`${BASE_URL}${todo._id}`, {
+        method: "PUT",
+        headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
+        body: JSON.stringify(todo)
+    }, {mode: "cors"})
+    .then(res => res.json());
+  }
