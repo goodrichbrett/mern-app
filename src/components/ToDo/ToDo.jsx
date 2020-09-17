4b73145fc56db28a4d8faa02f4a55c10d2a05842
goodrichbrett <brettgoodrich@outlook.com>
2020-09-17 11:45:58 -0500

    ?

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
-import { Link } from "react-router-dom";
-import { Button } from "reactstrap";
+import React from 'react';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 const ToDo = ({ name, isDone, dueDate }) => {
-  return (
-    <div>
-      <Card>
-        <CardBody>
-          <CardTitle>{name}</CardTitle>
-          <CardText>
-            IsDone: {isDone ? "true" : "false"} Due: {dueDate}
-          </CardText>
-        </CardBody>
-        <Link to={"/"}>
-          <Button color="primary">Delete</Button>
-        </Link>
-        <Link to={"/"}>
-          <Button color="primary">Update</Button>
-        </Link>
-      </Card>
-    </div>
-  );
+	return (
+		<div>
+			<Card>
+				<CardBody>
+					<CardTitle>{name}</CardTitle>
+					<CardText>
+						IsDone: {isDone ? 'true' : 'false'} Due: {dueDate}
+					</CardText>
+				</CardBody>
+				<Link to={'/'}>
+					<Button color="primary">Delete</Button>
+				</Link>
+				<Link to={'/'}>
+					<Button color="primary">Update</Button>
+				</Link>
+			</Card>
+		</div>
+	);
 };
 
 export default ToDo;
