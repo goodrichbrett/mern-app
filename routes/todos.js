7ed3469fe00f79d6478e8ca5650567e4945b3a7f
goodrichbrett <brettgoodrich@outlook.com>
2020-09-17 08:46:35 -0500

    add checkauth function to todos router

--- routes/todos.js
+++ routes/todos.js
@@ -6,6 +6,11 @@ router.get('/todos', todosCtrl.index);
 router.get('/todos/:id', todosCtrl.show);
 router.post('/todos', todosCtrl.create);
 router.put('/todos/:id', todosCtrl.update);
-router.delete('/todos/:id', todosCtrl.delete)
+router.delete('/todos/:id', todosCtrl.delete);
+
+function checkAuth(req, res, next) {
+	if (req.user) return next();
+	return res.status(401).json({ msg: 'Not Authorized' });
+}
 
 module.exports = router;
